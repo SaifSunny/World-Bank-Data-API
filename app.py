313466b3eca36156399b5a1717d78bd50238f438
unknown <saifsunny56@gmail.com>
2024-01-09 01:11:44 +0600

    first commit

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from app import create_app
 app = create_app()
 app.static_folder = 'static'
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
     # app.run(host="192.168.0.242", debug=True, port=8000)
